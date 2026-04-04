355238b4c9c07660493060a4811ae47b2b6cc4e9
Silvano <godonousilvano@gmail.com>
2026-04-04 12:52:15 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ process.on('unhandledRejection', (reason) => console.error('ğŸ’¥ Promesse rejetÃ
 // ==================== SERVICES EXTERNES ====================
 const ia = new Mistral({ apiKey: 'O2zJ5zADkoYVagGOR52tkxXrQFZ9SqQw' });
 const supabase = createClient('https://qzdalzdgwnundyafardl.supabase.co', 'sb_publishable_o0UzZ3WiSqn-G9jN1IG_AA_Bk4nef6g');
-const admin = ["120363407014174901@g.us"];
+const admin = ["22994847187@s.whatsapp.net"];
 
 // ==================== UTILS ====================
 const delay = ms => new Promise(res => setTimeout(res, ms));
